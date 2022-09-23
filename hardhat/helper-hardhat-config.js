eb07f690c95dbea3b3b27011af1fb1d02bda2879
Wmengti <0x3ceth@gmail.com>
2022-09-23 14:15:56 +0800

    add api

--- hardhat/helper-hardhat-config.js
+++ hardhat/helper-hardhat-config.js
@@ -2,7 +2,7 @@ const developmentChains = ['hardhat', 'localhost'];
 const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
 const whitelistAddress = '../Dapps/constants/WLaddress.json';
 const whitelistAbi = '../Dapps/constants/WLabi.json';
-const METADATA_URL = 'https://nft-collection-sneh1999.vercel.app/api/';
+const METADATA_URL = 'smart-dapps-dapps-j97c.vercel.app/api/';
 
 const crpytodevsAddress = '../Dapps/constants/CDaddress.json';
 const crpytodevAbi = '../Dapps/constants/CDabi.json';
