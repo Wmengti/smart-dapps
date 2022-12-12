a843dd37c146bde7f18b16c6e96123a23df114b7
Wmengti <0x3ceth@gmail.com>
2022-12-12 21:20:54 +0800

    dao twitter share3

--- Dapps/pages/dao_vote/index.js
+++ Dapps/pages/dao_vote/index.js
@@ -19,29 +19,19 @@ function DAOProposalVote(props) {
         <meta name='twitter:card' content='summary' />
         {/* <meta name='twitter:card' content='summary_large_image' /> */}
         <meta name='twitter:site' content='@pangmadee' />
-        <meta name='twitter:title' content='0x3c test web3' />
-        <meta name='twitter:description' content='Twitter share card' />
-        <meta name='twitter:url' content='https://0x3c.xyz/dao_vote' />
-        <meta property='og:url' content='https://0x3c.xyz/dao_vote' />
+        <meta name='twitter:title' content='0x3c test sub page web3' />
+        <meta name='twitter:description' content='Twitter share card subpage' />
+        {/* <meta name='twitter:url' content='https://0x3c.xyz/dao_vote' />
+        <meta property='og:url' content='https://0x3c.xyz/dao_vote' /> */}
         <meta name='twitter:type' content='article' />
         <meta
           name='twitter:image'
           content='https://farm6.staticflickr.com/5510/14338202952_93595258ff_z.jpg'
         />
-        <link rel='canonical' href='https://0x3c.xyz/dao_vote' />
         <link rel='icon' href='/entericon.png' />
       </Head>
 
       <DAOvoteList proposals={props.proposals} />
-      <TwitterShareButton
-        url={'https://0x3c.xyz' + router.asPath}
-        title='title'
-        via='pangmadee'
-        hashtags={['0x3c', '0xcreater']}
-        related={['0x3c']}
-      >
-        tweet
-      </TwitterShareButton>
     </div>
   );
 }
