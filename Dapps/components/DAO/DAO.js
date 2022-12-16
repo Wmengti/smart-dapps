b1cb4f6b1e83bea7bed17eaeeed3b0a21d059af1
Wmengti <0x3ceth@gmail.com>
2022-12-16 15:56:08 +0800

    change

--- Dapps/components/DAO/DAO.js
+++ Dapps/components/DAO/DAO.js
@@ -79,11 +79,7 @@ const DAO = () => {
             </div>
             <button
               className={styles.dao_btn_advice}
-              onClick={() =>
-                Router.push(
-                  '/FsAj7QT2Szg8UKAcu76Z/0xb1BfB47518E59Ad7568F3b6b0a71733A41fC99ad'
-                )
-              }
+              onClick={() => Router.push('/votefordao/vote')}
             >
               <div className={styles.dao_vote_proposal}>
                 <svg
